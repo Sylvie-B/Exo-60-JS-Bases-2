112c5c29ab385deed45af75b5c53ef655be9d29f
Sylvie-B <unsolo@hotmail.fr>
2020-12-01 11:11:18 +0100

    avec .listStyleType dans li

--- exo 1/appExo60.js
+++ exo 1/appExo60.js
@@ -1,12 +1,14 @@
+let objList = document.getElementsByTagName("ul");
+console.log(objList);
+
 let itemList = document.getElementsByTagName("li");
 console.log(itemList.length);
 
 for (let i = 0; i < itemList.length; i++){
     let item = itemList[i];
     item.innerText = "blabla";
-    console.log(i);
+    item.style.listStyleType = "circle";
 }
 
-let objList = document.getElementsByTagName("ul");
-console.log(objList);
-objList.style.listStyleType = "circle";
+
+
